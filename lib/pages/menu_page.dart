7dcd87870c7dccc4e0188d35a73a59fc00f89e2c
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2023-09-24 14:22:39 +0700

    Create Intro Page

---
+++ lib/pages/menu_page.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+
+  @override
+  State<MenuPage> createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+      ),
+    );
+  }
+}
